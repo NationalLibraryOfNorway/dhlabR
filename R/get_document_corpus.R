c172a407028335a0553a162685bf886dab46ea20
andrekaa <andre.kasen@nb.no>
2023-01-06 08:14:32 +0100

    default vals in doc corpus

--- R/get_document_corpus.R
+++ R/get_document_corpus.R
@@ -23,7 +23,7 @@
 #' @import httr
 #'
 #' @export
-get_document_corpus <- function(author, ddk, doctype, freetext, from_timestamp, from_year, fulltext, lang, limit, order_and_limit_by_rank, publisher, subject, title, to_timestamp, to_year){
+get_document_corpus <- function(doctype='digibok', author=NULL, ddk=NULL, freetext=NULL, subject=NULL, from_timestamp=NULL, to_timestamp=NULL, publisher=NULL, limit=10, order_and_limit_by_rank=NULL, title=NULL, from_year=NULL, to_year=NULL, fulltext=NULL, lang='nob'){
 
     url <- "https://api.nb.no/dhlab/build_corpus"
 
