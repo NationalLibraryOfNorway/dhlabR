37f6dda88b0b53e1c5c383964f00618349053a6a
andrekaa <andre.kasen@nb.no>
2023-02-17 09:16:35 +0100

    added ngram exp

--- R/get_ngram_from_newspapers.R
+++ R/get_ngram_from_newspapers.R
@@ -19,12 +19,12 @@
 #' @import httr
 #'
 #' @export
-get_ngram_from_books <- function(city = NULL, ddk = NULL, lang = NULL, period = list(), publisher = NULL, title = NULL, topic = NULL, word = list("hus", "blokk")){
+get_ngram_from_newspapers <- function(city = NULL, ddk = NULL, lang = NULL, period = list(), publisher = NULL, title = NULL, topic = NULL, word = list("hus", "blokk")){
     
-    ngram_book <- "https://api.nb.no/dhlab/ngram_book"
+    ngram_newspapers <- "https://api.nb.no/dhlab/ngram_newspapers"
     
     params <- list("city" = city, "ddk" = ddk, "lang" = lang, "period" = period, "publisher" = publisher, "title" = title, "topic" = topic, "word" = word)
-    query <- POST(ngram_book, body = params, encode = "json")
+    query <- POST(ngram_newspapers, body = params, encode = "json")
     
     return(stack(content(query)))
     
