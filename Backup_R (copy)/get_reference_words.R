f4638b831839734ea2e3434bd341bc9d8f77a5f0
LarsM <larsmtungland@gmail.com>
2023-03-24 18:00:58 +0100

    move some functions to other folder for isolated testing

--- Backup_R (copy)/get_reference_words.R
+++ Backup_R (copy)/get_reference_words.R
@@ -12,7 +12,7 @@
 #'
 #' @md
 #'
-#' @seealso 
+#' @seealso
 #'
 #' @examples
 #'
@@ -20,12 +20,12 @@
 #'
 #' @export
 get_reference_words <- function(doctype = NULL, from_year = NULL, to_year = NULL, words = NULL){
-    
+
     url <- "https://api.nb.no/dhlab/reference_words"
-    
+
     params <- list("doctype" = doctype, "from_year" = from_year, "to_year" = to_year, "words" = words)
     query <- POST(url, body = params, encode = "json")
-    
+
     return(content(query))
-    
+
 }
